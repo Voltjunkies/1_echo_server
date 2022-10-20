795179dbd88e14b15502781755ede7207906ebe3
Voltjunkies <113098673+Voltjunkies@users.noreply.github.com>
2022-10-20 12:06:50 +0000

    added debug prints

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket
 
 sock = socket.socket()
+print("Запускаем сервер")
 sock.bind(('', 9090))
+print("Начинаем слушать порт")
 sock.listen(0)
+print("Подключаем клиентов")
 conn, addr = sock.accept()
 print(addr)
 
 msg = ''
 
 while True:
+	print("Принимаем данные от клиента по 1КБ")
 	data = conn.recv(1024)
 	if not data:
+		print("Остановка подключения")
 		break
 	msg += data.decode()
+	print("Отправляем данные клиенту")
 	conn.send(data)
 
+print("Суммированное сообщение:")	
 print(msg)
-
+print("Останавливаем сервер")
 conn.close()
